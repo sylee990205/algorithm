414610c24915bb1957e609a566b3f42a3f38bd75
Soyoung <126841515+sylee990205@users.noreply.github.com>
2023-04-03 21:55:00 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 432 ms, Memory: 20988 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bufferedReader.readLine());
+        String star = "*";
+        for (int i = 1; i <= num; i++) {
+            for (int j = num-i; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = num-1; i >= 1; i--) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
